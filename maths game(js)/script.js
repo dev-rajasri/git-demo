95c24879e9a2b9d16e47e5b15782473ea4179e85
rajasri <dev.rajasri@gmail.com>
2017-09-19 18:29:04 +0530

    A simple maths multiplication game using script

---
+++ maths game(js)/script.js
@@ -0,0 +1,23 @@
+//if we click on start/reset
+  //if we are playing
+    //reload page
+  //if we are cot playing
+  //set score to 0
+    //show countdownbox
+    //reduce time by 1sec in loops
+       //time left?
+       //yes->continue
+       //no->Game over
+    //cannge button to reset
+    //generate new q&a
+
+ //if we click on answer box
+   //if we are playing
+     //correct?
+      //yes
+         //increase score
+         //show correct box for 1 sec
+         //generate new q&a
+      //no
+        //show try again box for 1 sec
+        
